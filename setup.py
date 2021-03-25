6138011c54680762f994d68d31b0d6678b548666
Ramakrishnan <ramakrishnan.h.1995@gmail.com>
2021-03-25 23:39:35 +0530

    Updated version for minor changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ INSTALL_REQUIRES = [
 ]
 
 setup(name='mould',
-      version='0.0.1',
+      version='0.0.3',
       description='A minimalistic python templating library',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
